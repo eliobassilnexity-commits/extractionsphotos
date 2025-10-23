8b162fe35c808c72e06299b82aca3533a7059789
eliobassilnexity-commits <eliobassilnexity@gmail.com>
2025-10-23 17:37:48 +0200

    Update extractionsphotos.py

--- extractionsphotos.py
+++ extractionsphotos.py
@@ -185,7 +185,7 @@ if (st.session_state.uploaded_excel and st.session_state.uploaded_pdf
         elif nb_img_restantes == nb_lignes_plan * 2:
             st.success("✅ Vérification finale OK : 2 photos par désordre (toutes pages)")
         else:
-            st.error(f"❌ Incohérence détectée dans le nombre de photos par désordre. Corrige celà directement sur le projet Archipad, exporte à nouveau les rapports PDF et Excel, puis reviens ici pour le traitement."")
+            st.error(f"❌ Incohérence détectée dans le nombre de photos par désordre. Corrige celà directement sur le projet Archipad, exporte à nouveau les rapports PDF et Excel, puis reviens ici pour le traitement.")
             shutil.rmtree(output_folder)
             st.stop()
 
@@ -204,6 +204,7 @@ if st.session_state.extracted and st.session_state.zip_path is not None:
             file_name="Extraction_finale.zip",
             mime="application/zip"
         )
+
 
 
 
